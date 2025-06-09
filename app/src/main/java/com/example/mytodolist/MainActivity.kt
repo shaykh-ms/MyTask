bb278fee85d0b4e17238ac6a149cc9cf27ce4fb4
Mohd Shaya <shaykhms@gmail.com>
2025-06-09 20:34:40 +0530

    -> splash screen logic refactored.

--- app/src/main/java/com/example/mytodolist/MainActivity.kt
+++ app/src/main/java/com/example/mytodolist/MainActivity.kt
@@ -1,9 +1,6 @@
 package com.example.mytodolist
 
-import android.animation.ObjectAnimator
 import android.os.Bundle
-import android.view.View
-import android.view.animation.OvershootInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,9 +8,7 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import androidx.core.animation.doOnEnd
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.mytodolist.presentation.task_listing.TaskListingScreen
 import com.example.mytodolist.presentation.task_listing.TaskListingViewModel
 import com.example.mytodolist.ui.theme.MyTodoListTheme
@@ -25,35 +20,38 @@ class MainActivity : ComponentActivity() {
     private val viewModel by viewModels<TaskListingViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         installSplashScreen().apply {
             setKeepOnScreenCondition {
-                viewModel.state.isReady
+                val isReady = viewModel.state.isReady
+                !isReady
             }
-         /*   setOnExitAnimationListener { screen ->
-                val zoomX = ObjectAnimator.ofFloat(
-                    screen.iconView,
-                    View.SCALE_X,
-                    0.4f,
-                    0.0f
-                )
-                zoomX.interpolator = OvershootInterpolator()
-                zoomX.duration = 500L
-                zoomX.doOnEnd { screen.remove() }
 
-                val zoomY = ObjectAnimator.ofFloat(
-                    screen.iconView,
-                    View.SCALE_Y,
-                    0.4f,
-                    0.0f
-                )
-                zoomY.interpolator = OvershootInterpolator()
-                zoomY.duration = 500L
-                zoomY.doOnEnd { screen.remove() }
+            /*   setOnExitAnimationListener { screen ->
+                   val zoomX = ObjectAnimator.ofFloat(
+                       screen.iconView,
+                       View.SCALE_X,
+                       0.4f,
+                       0.0f
+                   )
+                   zoomX.interpolator = OvershootInterpolator()
+                   zoomX.duration = 500L
+                   zoomX.doOnEnd { screen.remove() }
 
-                zoomX.start()
-                zoomY.start()
+                   val zoomY = ObjectAnimator.ofFloat(
+                       screen.iconView,
+                       View.SCALE_Y,
+                       0.4f,
+                       0.0f
+                   )
+                   zoomY.interpolator = OvershootInterpolator()
+                   zoomY.duration = 500L
+                   zoomY.doOnEnd { screen.remove() }
 
-            }*/
+                   zoomX.start()
+                   zoomY.start()
+
+               }*/
 
         }
         enableEdgeToEdge()
--- app/src/main/java/com/example/mytodolist/presentation/task_listing/TaskListingViewModel.kt
+++ app/src/main/java/com/example/mytodolist/presentation/task_listing/TaskListingViewModel.kt
@@ -9,14 +9,9 @@ import androidx.lifecycle.viewModelScope
 import com.example.mytodolist.data.mapper.toTaskEntity
 import com.example.mytodolist.domain.repository.TaskRepository
 import com.example.mytodolist.util.Resource
-import com.example.mytodolist.util.analytics.AnalyticsEvent
-import com.example.mytodolist.util.analytics.AnalyticsLogger
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -25,24 +20,8 @@ class TaskListingViewModel @Inject constructor(
     private val todoRepository: TaskRepository
 ) : ViewModel() {
 
-    private val _isReady = MutableStateFlow(false)
-    val isReady = _isReady.asStateFlow()
-
-
-
 
     var state by mutableStateOf(TaskState())
-
-    init {
-        viewModelScope.launch {
-            delay(1000L)
-           // _isReady.value = true
-
-            state = state.copy(
-                isReady = true
-            )
-        }
-    }
 
     private val _uiEvent = MutableSharedFlow<TaskUiEvent>()
     val uiEvent = _uiEvent.asSharedFlow()
@@ -71,6 +50,7 @@ class TaskListingViewModel @Inject constructor(
 
                             state = state.copy(
                                 isLoading = false,
+                                isReady = true,
                                 tasks = listings.toMutableStateList(),
                             )
                             onUiEvent(TaskUiEvent.ShowEmptyView(isShow = listings.isEmpty()))
